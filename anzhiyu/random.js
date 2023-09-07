9d729e5ebd9825bbc83b1bdc7d260f9872d1cd55
aek971noob <1751897372@qq.com>
2023-09-07 20:30:37 +0800

    Site updated: 2023-09-07 20:30:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/16107.html","posts/ddae.html","posts/ddae.html","posts/ddae.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/ddae.html","posts/ddae.html","posts/16107.html","posts/ddae.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
